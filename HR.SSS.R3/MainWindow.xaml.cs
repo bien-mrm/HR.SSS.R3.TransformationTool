a99eb94c60c16cc469a9d52d6dd3a0daa5106915
Bien Angelo Relucano <bien.relucano@mrm.com>
2022-03-18 06:09:59 +0800

    Initial UI and Initialization Setup

--- HR.SSS.R3/MainWindow.xaml.cs
+++ HR.SSS.R3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using HR.SSS.R3.Models;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,27 +22,65 @@ namespace HR.SSS.R3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static R3SessionContainer R3Session { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            R3Session = new R3SessionContainer();
         }
 
         private void BtnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            this.SetupSessionValues();
+
+
+        }
+
+        private void SetupSessionValues()
+        {
+            if (String.IsNullOrEmpty(TxtInputFile.Text))
+            {
+                MessageBox.Show("Please specify your source Excel file.", "Missing Source File");
+                return;
+            }
+            else
+            {
+                R3Session.InputFile = TxtInputFile.Text;
+            }
+
+            if (String.IsNullOrEmpty(TxtOutputFileName.Text))
+            {
+                MessageBox.Show("Please specify the name of your output R3 file.", "Unspecified Output File Name");
+                return;
+            }
+            else
+            {
+                R3Session.OutputFileName = TxtOutputFileName.Text;
+            }
+
+            if (String.IsNullOrEmpty(TxtEmployerName.Text))
+            {
+                MessageBox.Show("Please specify the Employer name.", "Unspecified Employer Name");
+                return;
+            }
+            else
+            {
+                R3Session.EmployerName = TxtEmployerName.Text;
+            }
 
         }
 
         private void BtnBrowseInput_Click(object sender, RoutedEventArgs e)
         {
-
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+
             if (openFileDialog.ShowDialog() == true)
             {
+                R3Session.InputFile = openFileDialog.FileName;
+                R3Session.InputDirectory = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+
                 TxtInputFile.Text = openFileDialog.FileName;
             }
         }
---
+++ HR.SSS.R3/Models/R3SessionContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.SSS.R3.Models
+{
+    public class R3SessionContainer
+    {
+        public string InputFile { get; set; }
+
+        public string InputDirectory { get; set; }
+
+        public string OutputFileName { get; set; }
+
+        public string EmployerName { get; set; }
+
+    }
+}
